4a87c11e2dfd8f94c873b760d08d70f44e070c7e
Arif Setyawan <me@arifsetyawan.com>
2020-10-24 23:26:55 +0800

    add utilities function
    convertNumber, convertNumberafterComma, digitToUnit, numberToText,  and struct Setting

--- terbilang.go
+++ terbilang.go
@@ -1,13 +1,32 @@
 package terbilang
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
-func toWord(someNumber float64) string {
+func convertNumber(someNumber int) string {
 	var response string = ""
 	var printUnit bool = true
 	var isBelasan bool = false
 
 	return response
+}
+
+func convertNumberafterComma(number int) string {
+
+	letterOfNumber := strconv.Itoa(number)
+	arrayOfLetter := strings.Split(letterOfNumber, "")
+	arrayOfWord := []string{}
+
+	for _, i := range arrayOfLetter {
+		j, _ := strconv.Atoi(i)
+		arrayOfWord = append(arrayOfWord, numberToText(j))
+	}
+
+	return strings.Join(arrayOfWord, " ")
 }
 
 func digitToUnit(digit float64) string {
@@ -24,5 +43,41 @@ func digitToUnit(digit float64) string {
 	}
 
 	return returnUnit
+}
 
+func numberToText(index int) string {
+	numbers := []string{"satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"}
+	return numbers[index-1]
+}
+
+type Setting struct {
+	decimal string
+}
+
+func ToWord(targetNumber float64, setting Setting) string {
+	var result string = ""
+
+	if setting == (Setting{}) {
+		setting.decimal = "."
+	}
+
+	strOfTargetNumber := strconv.FormatFloat(targetNumber, 'f', 6, 64)
+	numberComponent := strings.Split(strOfTargetNumber, setting.decimal)
+
+	majorSegment, errStrConvert := strconv.Atoi(numberComponent[0])
+	if errStrConvert != nil {
+		panic(errStrConvert)
+	}
+
+	result = convertNumber(majorSegment)
+
+	if len(numberComponent) == 2 {
+		afterCommaSegment, errStrConvert := strconv.Atoi(numberComponent[1])
+		if errStrConvert != nil {
+			panic(errStrConvert)
+		}
+		result = fmt.Sprintf("%s koma %s", result, convertNumberafterComma(afterCommaSegment))
+	}
+
+	return result
 }
