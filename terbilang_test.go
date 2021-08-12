28be95951682263722a119ce854a941375a7edbf
Arif Setyawan <me@arifsetyawan.com>
2021-08-12 14:52:42 +0000

    add asert in other test

--- terbilang_test.go
+++ terbilang_test.go
@@ -22,14 +22,17 @@ func TestConvertNumber(t *testing.T) {
 func TestFromInt(t *testing.T) {
 	result := FromInt{Value: 2000}.ToWord()
 	fmt.Println(result)
+	assert.True(t, result == "dua ribu")
 }
 
 func TestFromFloat(t *testing.T) {
 	result := FromFloat{Value: 2150000.00001}.ToWord()
 	fmt.Println(result)
+	assert.True(t, result == "dua juta seratus lima puluh ribu koma nol nol nol nol satu nol")
 }
 
 func TestFromString(t *testing.T) {
 	result := FromString{"15150"}.ToWord()
 	fmt.Println(result)
+	assert.True(t, result == "limabelas ribu seratus lima puluh")
 }
